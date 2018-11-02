cdd7776769657b4e4af8090493f9ef5df4097b97
MP5030749 <MP5030749@SYNPS6415.syntelorg.com>
2018-11-02 11:45:55 +0530

    Removed systax error

--- GFBO/src/GFBO.java
+++ GFBO/src/GFBO.java
@@ -4,7 +4,7 @@ public class GFBO {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Sysout("GFBO Application");
+		System.out.println("GFBO Application");
 
 	}
 
